fef81be24afb3c236bb49d7b80f8c091013b71c4
coall132 <kaeligroche132@gmail.com>
2024-09-16 23:36:47 +0200

    a

--- kaelig/auth_user/views.py
+++ kaelig/auth_user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .forms import UserForm
+from django.http import HttpResponse
 
 def show_login(request):
     return render(request, 'login.html')
@@ -13,16 +14,24 @@ def sign_in(request):
     if request.method=='POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            if User.objects.filter(username=UserForm.username).exists():
+            # Récupérer les données validées via cleaned_data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Vérifier si l'utilisateur existe déjà
+            if User.objects.filter(username=username).exists():
                 messages.error(request, "Ce nom d'utilisateur est déjà pris.")
             else:
                 # Créer l'utilisateur
-                user = User.objects.create_user(username=UserForm.username, password=UserForm.password)
+                user = User.objects.create_user(username=username, password=password)
                 user.save()
             
             # Message de succès et redirection
                 messages.success(request, "Utilisateur créé avec succès.")
                 return redirect('success')
+        else:
+            return HttpResponse("Hello, World!")
+    return render(request, 'sign.html', {'form': form})
 
 def success(request):
     return render(request, 'sucess.html')
