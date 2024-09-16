167d9609f14f2177e49a69259cfcc8223137c1f1
coall132 <kaeligroche132@gmail.com>
2024-09-16 16:47:58 +0200

    fin test

--- kaelig/autoML/views.py
+++ kaelig/autoML/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 def home(request):
-    return render(request,'accueil.html')
+    return render(request, 'accueil.html')
 
 def login(request):
-    return render(request,'login.html')
+    return render(request, 'login.html')
+
+def sign(request): 
+    return render(request, 'sign.html')
 # Create your views here.
--- kaelig/kaelig/urls.py
+++ kaelig/kaelig/urls.py
@@ -21,5 +21,6 @@ from autoML import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('',views.login, name='login')
+    path('login/',views.login, name='login'),
+    path('sign/', views.sign, name='sign')
 ]
