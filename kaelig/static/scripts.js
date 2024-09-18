e0671c873e5e0ee201df4c636b450c7415817c3c
coall132 <kaeligroche132@gmail.com>
2024-09-18 10:32:13 +0200

    a

--- kaelig/static/scripts.js
+++ kaelig/static/scripts.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const drag_object = document.getElementById("object_draggable");
     const zone_drag = document.getElementById('zone_drag');
+    const positionXzone=zone_drag.offsetLeft;
+    const positionYzone=zone_drag.offsetTop;
 
     // Gérer le début du drag (dragstart)
     drag_object.addEventListener('dragstart', (e) => {
@@ -16,16 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
     // Gérer le drop
     zone_drag.addEventListener('drop', (e) => {
         e.preventDefault();
+        var nb_clone= document.getElementsByClassName('clone')
         let positionX=e.clientX
         let positionY=e.clientY
+        let positionXfinal=positionX-positionXzone
+        let positionYfinal=positionY-positionYzone
         const id = e.dataTransfer.getData('text'); // Récupère l'ID de l'élément
         const draggedElement = document.getElementById(id);
 
         // Crée un clone de l'élément déplacé
         const clone = draggedElement.cloneNode(true);
         clone.classList.add('clone'); // Ajoute une classe au clone (si nécessaire)
+        clone.setAttribute("style", `left:${positionXfinal}px;top:${positionYfinal}px;position:relative`);
 
         // Ajoute le clone dans la zone de drop
         zone_drag.appendChild(clone);
     });
-  });
+});
