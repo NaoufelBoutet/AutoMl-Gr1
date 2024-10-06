2356edf37ab31cc8946bbd7869e1af5756bfb25c
NaoufelGreta <naoufelboutet2@gmail.com>
2024-10-06 16:03:14 +0200

    secondtry

--- WebSite/Accueil/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render , redirect
-from django.http import HttpResponse
-
-# Create your views here.
-
-
-
-def index(request):
-    return render(request,"Accueil.html")
--- WebSite/Auth/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def connexion(request):
-    return render(request,"connexion.html")
-
-def inscription(request):
-    return render(request,"inscription.html")
