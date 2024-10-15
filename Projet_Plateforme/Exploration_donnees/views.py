cbba8b8b9ca1c1caaf1dfbce12138d21fc0a58a6
L. Berville <leabizbille@hotmail.com>
2024-10-15 15:54:05 +0200

    Importation et message

--- Projet_Plateforme/Exploration_donnees/views.py
+++ Projet_Plateforme/Exploration_donnees/views.py
@@ -5,39 +5,38 @@ import csv
 #from django.contrib import messages
 from .models import Projet
 from .forms import ProjetForm
+import os  # Nécessaire pour gérer les chemins de fichiers
 
 
 def hello(request):
     utilisateurs = Utilisateurs.objects.all()
-    return render(request, 'Exploration_donnees/hello.html',
-        {'utilisateurs': utilisateurs})
+    return render(request, 'Exploration_donnees/hello.html', {'utilisateurs': utilisateurs})
+
+from django.contrib import messages
 
 def import_csv(request):
     if request.method == 'POST':
         form = ProjetForm(request.POST, request.FILES)
         if form.is_valid():
-            csv_file = form.save()
-            print('succes')
-            # Vérification du type de fichier
-            """if not csv_file.nom_projet.endswith('.csv'):
+            projet = form.save()
+            messages.success(request, 'Fichier téléchargé avec succès')
+            csv_path = projet.csv_file.path
+            if not csv_path.endswith('.csv'):
                 messages.error(request, 'Ce fichier n\'est pas un fichier CSV.')
-                return redirect('import_csv')"""
-
-            # Lire le fichier CSV
-            """try:
-                reader = csv.reader(csv_file.read().decode('utf-8').splitlines())
-                next(reader)  # Ignorer la ligne d'en-tête
-                for row in reader:
-                    # Assurez-vous que le fichier CSV contient les colonnes nécessaires
-                    _, created = Projet.objects.get_or_create(
-                        name=row[0],
-                    )
+                return redirect('import_csv')
+            try:
+                with open(csv_path, newline='', encoding='utf-8') as csvfile:
+                    reader = csv.reader(csvfile)
+                    next(reader)
+                    for row in reader:
+                        print(f"Nom du projet : {row[0]}")
                 messages.success(request, 'Fichier CSV importé avec succès !')
                 return redirect('import_csv')
             except Exception as e:
                 messages.error(request, f'Erreur lors de l\'importation : {str(e)}')
-                return redirect('import_csv')"""
+                return redirect('import_csv')
     else:
         form = ProjetForm()
 
     return render(request, 'import_csv.html', {'form': form})
+
