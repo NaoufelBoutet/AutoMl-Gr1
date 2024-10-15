033a6f7251d3b05fbba61713203895c0dd2e6e24
NaoufelGreta <naoufelboutet2@gmail.com>
2024-10-15 13:59:56 +0200

    'authend'

---
+++ mysite/authentification/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from .form import ConnexionForm,InscriptionForm
+from django.contrib.auth import login, logout, authenticate
+# Create your views here.
+
+
+def connexion(request):
+    form = ConnexionForm()
+    if request.method == 'POST':
+        form = ConnexionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print("Connexion réussie")
+                return redirect('accueil')
+            else:
+                form.add_error(None, "Nom d'utilisateur ou mot de passe incorrect.")
+                return redirect("connexion")
+
+    return render (request, "connexion.html",{"form":form})
+
+def deconnexion(request):
+    logout(request)
+    return render(request,"connexion.html")
+
+def inscription(request):
+    if request.method == 'POST':
+        form = InscriptionForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)  # Connecte automatiquement l'utilisateur après l'inscription
+            return redirect('accueil')  # Redirige vers la page d'accueil après l'inscription
+    else:
+        form = InscriptionForm()
+
+    return render(request, 'inscription.html', {'form': form})
+    
+
