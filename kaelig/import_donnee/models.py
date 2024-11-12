71b594d62d4945f0f8e336a8532d55691aaa5bae
coall132 <kaeligroche132@gmail.com>
2024-11-12 12:59:43 +0100

    a

--- kaelig/import_donnee/models.py
+++ kaelig/import_donnee/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class File(models.Model):
+    user = models.Charfield()
+    name = models.Charfield(max_length=100)
+    metadata = models.CharField
+    def __str__(self):
+        return self.name
