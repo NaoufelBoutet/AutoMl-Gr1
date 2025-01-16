1331db23c58bda2ed01cf0aa5729b621222a0edb
coall132 <kaeligroche132@gmail.com>
2025-01-16 15:36:57 +0100

    a

--- src/auth_user/models.py
+++ src/auth_user/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    id = models.AutoField(primary_key=True)
-
-    def __str__(self):
-        return self.name
